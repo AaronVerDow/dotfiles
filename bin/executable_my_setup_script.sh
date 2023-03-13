3e1d28222a6b60ffcc5cd5f725e50e00d4db5810
averdow <averdow@uba46d324df0d58.ant.amazon.com>
2023-03-13 11:03:59 -0500

    add arc icon theme for battery widget

--- bin/executable_my_setup_script.sh
+++ bin/executable_my_setup_script.sh
@@ -17,6 +17,19 @@ fi
 
 
 if [ -f /etc/debian_version ]; then
+
 	# enable dark mode in Ubuntu
 	gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
+	# install Arc icons (in aur for Arch)
+	# used for battery tray 
+	if ! [ -d ~/.icons/Arc ]; then
+		mkdir -p ~/.icons/Arc
+		( 
+			cd /tmp
+			git clone https://github.com/horst3180/arc-icon-theme.git
+			# widget is hard coded to this path, won't detect theme in ~/.icons
+			sudo mv arc-icon-theme/Arc /usr/share/icons
+		)
+	fi
 fi
