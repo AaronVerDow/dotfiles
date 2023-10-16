d20ac87363ca538bbeafede96e17ae70d242b5a7
averdow <averdow@amazon.com>
2023-10-16 10:03:12 -0500

    multi head wallpaper

--- bin/executable_wallpaper.sh
+++ bin/executable_wallpaper.sh
@@ -1,1 +1,3 @@
-nitrogen --random ~/Pictures/mah_wallpapers/ --set-zoom-fill
+nitrogen --random ~/Pictures/mah_wallpapers/ --set-zoom-fill --head=0
+nitrogen --random ~/Pictures/mah_wallpapers/ --set-zoom-fill --head=1 || true
+nitrogen --random ~/Pictures/mah_wallpapers/ --set-zoom-fill --head=2 || true
